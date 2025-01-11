3080a5189f25af3b9134eaafd9490e05e4c41f39
Prakhar Jaiswal <jaiswalprakhar98@gmail.com>
2025-01-12 03:19:41 +0530

    Creating Signup Screen Backend Apis

---
+++ app.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const dotenv = require('dotenv');
+const fs = require('fs');
+const express = require('express');
+dotenv.config({ path: './.env' });
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const morgan = require('morgan');
+
+const errorController = require("./controllers/error");
+const sequelize = require('./util/database');
+
+const User = require('./models/user');
+
+const Port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+
+const userRoutes = require('./routes/user');
+
+/*const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, 'access.log'),
+    { flags: 'a' }
+);*/
+
+//app.use(morgan('combined', { stream: accessLogStream }));
+
+app.use(express.json());
+
+app.use('/user', userRoutes);
+
+app.use((req, res) => {
+    const urlWithoutQuery = req.path; // Extract only the path without query parameters
+    res.sendFile(path.join(__dirname, `public/${urlWithoutQuery}`));
+});
+
+//Error Handle for throwing errors manually
+app.use((err, req, res, next) => {
+    //console.error(err.stack);
+    const statusCode = err.statusCode || 500;
+    //console.log(err.errors[0].type);
+
+    res.status(statusCode).json({ 
+        err: err,
+        message: err.message,
+        status: statusCode >= 400 && statusCode < 500 ? 'fail' : 'error',
+        success: false
+    });
+});
+
+app.use(errorController.get404);
+
+sequelize.sync()
+.then(result => {
+    app.listen(Port, () => {
+        console.log(`Server listening at PORT ${Port}`);
+    });
+})
+.catch(err => {
+    console.log(err);
+})
---
+++ models/user.js
@@ -0,0 +1,56 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
+
+const User = sequelize.define('user', {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    fullName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            noNumbers(value) {
+                if (/\d/.test(value)) {
+                    throw new Error('Name should not contain numbers');
+                }
+                else if(value === "") {
+                  throw new Error('Name cannot be empty');
+                }
+            }
+        }
+    },
+    emailId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: {
+            args: true,
+            msg: 'Email ID already exists'
+        },
+        validate: {
+            isEmail: {
+                args: true,
+                msg: 'Enter a valid Email Address'
+            }
+        }
+    },
+    phoneNumber: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+            customValidator(value) {
+              if (value.length !== 10) {
+                throw new Error("Phone Number should be 10 digits long");
+              }
+            }
+        }
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = User;
